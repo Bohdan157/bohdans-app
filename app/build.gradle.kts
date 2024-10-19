f2cd7a5df326af9a337089e0ea3e7c038729813c
Bohdan <121731955+Bohdan157@users.noreply.github.com>
2024-10-19 22:08:56 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.bohdan157.app"
         minSdk = 28
         targetSdk = 34
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
